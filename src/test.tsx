c4a39bd9fde1f4dd9e66101a712e7aaf28f80279
BrianOtaku <Brainotaku11@gmail.com>
2024-07-12 19:29:27 +0700

    fisrt commit

---
+++ src/test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './hello.css';
+const Hello = () => {
+    return (
+        <div className='parentElement'>
+            <div className='childrenElement'>
+                <h1>hadgsfigfeiu</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Hello
