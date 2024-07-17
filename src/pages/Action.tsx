cab649b87508d8ee4e1ad5312d67567ac222e628
dinhnguyen888 <nguyenphucdinh89@gmail.com>
2024-07-17 21:25:09 +0700

    test

--- src/pages/Action.tsx
+++ src/pages/Action.tsx
@@ -5,7 +5,7 @@ const Action: React.FC = () => {
     return (
         <div>
             <h1>Action Page</h1>
-            <p>hello this is absolutely Action page content.</p>
+            <p>alo Action page content.</p>
             <Image src="./images/banner-10.png" fluid />
         </div>
     );
