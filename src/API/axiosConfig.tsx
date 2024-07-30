cff239c8303fdda59d3e17d6f8f30d3f8a73e050
dinhnguyen888 <nguyenphucdinh89@gmail.com>
2024-07-30 20:19:07 +0700

    fix some connection

--- src/API/axiosConfig.tsx
+++ src/API/axiosConfig.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const AxiosInstance = axios.create({
-  baseURL: 'https://localhost:7161/api/News',
+  baseURL: 'https://localhost:7161/api/',
   headers: {
     'Content-Type': 'application/json',
   },
